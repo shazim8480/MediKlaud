a34c576f3826ba8c6294e659838478add1081a10
shazim8480 <shazim_99@yahoo.com>
2022-08-03 23:32:27 +0600

    resources updated

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { NativeBaseProvider, Box } from "native-base";
+
+import Header from "./src/components/shared/Header";
+import DashboardScreen from "./src/screens/DashboardScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <SafeAreaView style={styles.container}>
+        <Header />
+        <DashboardScreen />
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
---
+++ constants/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+  background: "#D9D9D9",
+  primary: "#036BC6",
+  secondary: "#CD2237",
+  body: "#474747",
+};
